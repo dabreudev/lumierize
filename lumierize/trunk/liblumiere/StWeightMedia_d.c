28afca3dd3a6d9c4395e9811de0cb67fe1de0ad0
cquike <cquike@6825285c-ca2a-11de-9227-81ff81aedbed>
2010-03-14 22:28:35 +0000

    Moved to liblumiere

---
+++ lumierize/trunk/liblumiere/StWeightMedia_d.c
@@ -0,0 +1,24 @@
+#include "modulos.h"
+
+/* //De Javier Gorgas */
+
+double StWeightMedia_d(int n, double *x, double *w,double *sigma)
+{
+  int i;
+  double m=0,s=0;
+  double sumw=0;
+
+
+  for(i=0;i<n;i++) {
+    m+=x[i]*w[i];
+    sumw+=w[i];
+  }
+
+
+  m=m/sumw;
+  for(i=0;i<n;i++) {
+    s+=(x[i]-m)*(x[i]-m)*w[i];
+  }
+  *sigma=sqrt(n*s/((n-1.)*sumw));
+  return(m);
+}
